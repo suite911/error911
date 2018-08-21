31d57e8043cf3bbcc0a49549b984a241fa1f67f8
Amy <amyadzuki@gmail.com>
2018-08-21 00:50:06 +0000

    Update errors.go

--- errors.go
+++ errors.go
@@ -28,6 +28,28 @@ func (err Cancel) Error() string {
 	return err.err.Error()
 }
 
+// error911.Email
+
+type Email struct {
+	err error
+}
+
+func NewEmail(msg ...interface{}) error {
+	return new(Email).Init(msg...)
+}
+
+func (err *Email) Init(msg ...interface{}) error {
+	err.err = pkgErrors.Wrap(
+		errors.New("An e-mail address was invalid"),
+		fmt.Sprint(msg...),
+	)
+	return err
+}
+
+func (err Email) Error() string {
+	return err.err.Error()
+}
+
 // error911.NotSupported
 
 type NotSupported struct {
