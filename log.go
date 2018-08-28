0b49b4a8ffff9d3792e24992c28dcf2aab9dd38d
Amy <amyadzuki@gmail.com>
2018-08-28 22:48:07 +0000

    Update log.go

--- log.go
+++ log.go
@@ -17,31 +17,3 @@ func (l *Log) Init(title string) *Log {
 func (l *Log) Append(language, subTitle string, msg ...interface{}) {
 	l.Entries = append(l.Entries, NewLogEntry(language, subTitle, msg...))
 }
-
-func (l Log) HTML() string {
-	s := "<h2>" + l.Title + "</h2>\n"
-	for _, e := range l.Entries {
-		s += e.HTML()
-		s += "\n"
-	}
-	return s
-}
-
-func (l Log) MarkDown() string {
-	s := "## " + l.Title + " ##\n"
-	for _, e := range l.Entries {
-		s += e.MarkDown()
-		s += "\n"
-	}
-	return s
-}
-
-func (l Log) Text() string {
-	s := "=== \"" + l.Title + "\"\n"
-	for _, e := range l.Entries {
-		s += e.Text()
-		s += "\n"
-	}
-	s += "...\n"
-	return s
-}
