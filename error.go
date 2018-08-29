94317d7de7bf100656801d96b7ce5c776ed4a62f
Amy <amyadzuki@gmail.com>
2018-08-29 04:09:59 +0000

    Update error.go

--- error.go
+++ error.go
@@ -1,31 +1,18 @@
 package error911
 
-import (
-	"fmt"
-
-	"github.com/pkg/errors"
-)
-
 type Error *SError;
 
 func New(title string, cause error, msg ...interface{}) Error {
-	var err Error = new(SError)
-	return err.Init(title, cause, msg...)
+	return ImplNew(title, cause, msg...)
 }
 
 func (err Error) Init(title string, cause error, msg ...interface{}) Error {
-	if cause == nil {
-		cause = errors.New(title)
-	}
-	e := errors.Wrap(cause, fmt.Sprint(msg...))
-	err.err = e
-	return err
+	return ImplInit(err, title, cause, msg...)
 }
 
-func (err *Error) New(title string, cause error, msg ...interface{}) {
-	if *err == nil {
-		*err = New(title, cause, msg...)
-	} else {
-		*err = errors.Wrap((*err).err, "\uff62" + fmt.Sprintf("%q: %q", fmt.Sprint(msg...), cause) + "\uff63")
-	}
+func (err Error) New(title string, cause error, msg ...interface{}) Error {
+	return ImplNewMethod(err, title, cause, msg...)
+}
+func (err *Error) Push(title string, cause error, msg ...interface{}) {
+	ImplPush(err, title, cause, msg...)
 }
