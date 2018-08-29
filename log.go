742cf184ef7e5cd37a819e100780c2c56c2bdf67
Amy <amyadzuki@gmail.com>
2018-08-29 00:20:47 +0000

    Update log.go

--- log.go
+++ log.go
@@ -1,19 +1,36 @@
 package error911
 
+// Log references an error and contains a log
 type Log struct {
-	Title   string
 	Entries []*LogEntry
+	pError  *error
+	Title   string
 }
 
-func NewLog(title string) *Log {
-	return new(Log).Init(title)
+// Create a new Log and initialize it
+func NewLog(title string, pErr *error) *Log {
+	return new(Log).Init(title, pErr)
 }
 
-func (l *Log) Init(title string) *Log {
+// Initialize the Log
+func (l *Log) Init(title string, pErr *error) *Log {
+	l.pError = pErr
 	l.Title = title
 	return l
 }
 
+// Append an entry to the log
 func (l *Log) Log(language, subTitle string, msg ...interface{}) {
 	l.Entries = append(l.Entries, NewLogEntry(language, subTitle, msg...))
+}
+
+// Push an error onto the error stack
+func (l *Log) Push(msg ...interface{}) {
+	if l == nil {
+		panic("cannot push to nil error911.Log")
+	}
+	if l.pError == nil {
+		panic("cannot push to uninitialized error911.Log")
+	}
+	l.pError.Push(l.Title, msg...)
 }
