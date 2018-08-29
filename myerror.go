b5f1f62ff866e437558f5636542a66c24fd23510
Amy <amyadzuki@gmail.com>
2018-08-29 04:17:09 +0000

    Update and rename error.go to myerror.go

---
+++ myerror.go
@@ -0,0 +1,18 @@
+package error911
+
+type MyError *SError;
+
+func New(title string, cause error, msg ...interface{}) MyError {
+	return ImplNew(title, cause, msg...)
+}
+
+func (err MyError) Init(title string, cause error, msg ...interface{}) MyError {
+	return ImplInit(err, title, cause, msg...)
+}
+
+func (err MyError) New(title string, cause error, msg ...interface{}) MyError {
+	return ImplNewMethod(err, title, cause, msg...)
+}
+func (err *MyError) Push(title string, cause error, msg ...interface{}) {
+	ImplPush(err, title, cause, msg...)
+}
